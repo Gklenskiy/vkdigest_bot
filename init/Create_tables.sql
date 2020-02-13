6787e707aa12b64e3bca0403d38498bbe810d1cd
Grigoriy Klenskiy <gklensky@swiftcom.uk>
2020-02-13 22:00:01 +0300

    add create user_sources script

--- init/Create_tables.sql
+++ init/Create_tables.sql
@@ -2,3 +2,9 @@ create table users (
 	user_id integer constraint users_PK primary key,
 	token varchar(100) not null
 );
+
+create table user_sources (
+	user_id integer not null,
+	source varchar(100) not null,
+	UNIQUE (user_id, source)
+);
